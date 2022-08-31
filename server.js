a25d51226abed88b1352a7265a9fbac26907e688
Sarrthi Jasrotia <sarrthijasrotia05@gmail.com>
2022-08-30 20:53:11 -0500

    the drinks file is connected with the server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
+const drinks = require("./models/drinks")
 
-app.get("/welcome/", (req,res)=>{
+app.get("/", (req,res)=>{
     res.send("Welcome to the Gitpub App!");
 
 })
+
+app.get(":/")
 
 app.listen(3000, () => {
     console.log("Its working")
